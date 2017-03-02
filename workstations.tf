67abacede632958372cac3bf6de0069945d933e5
Nathen Harvey <nharvey@chef.io>
2017-03-02 06:00:52 -0800

    Two full decks of cards
    Signed-off-by: Nathen Harvey <nharvey@chef.io>

--- workstations.tf
+++ workstations.tf
@@ -2,7 +2,7 @@ module "heart-workstations" {
   source = "./workstations"
   suit = "hearts"
   color = "orange"
-  count = 2
+  count = 13
   automate_fqdn = "${aws_route53_record.automate.name}"
   aws_sshkey = "${var.aws_sshkey}"
   contact_tag = "${var.contact_tag}"
@@ -11,7 +11,68 @@ module "heart-workstations" {
 module "diamonds-workstations" {
   source = "./workstations"
   suit = "diamonds"
-  count = 2
+  color = "orange"
+  count = 13
+  automate_fqdn = "${aws_route53_record.automate.name}"
+  aws_sshkey = "${var.aws_sshkey}"
+  contact_tag = "${var.contact_tag}"
+}
+
+module "spades-workstations" {
+  source = "./workstations"
+  suit = "spades"
+  color = "orange"
+  count = 13
+  automate_fqdn = "${aws_route53_record.automate.name}"
+  aws_sshkey = "${var.aws_sshkey}"
+  contact_tag = "${var.contact_tag}"
+}
+
+module "clubs-workstations" {
+  source = "./workstations"
+  suit = "spades"
+  color = "orange"
+  count = 13
+  automate_fqdn = "${aws_route53_record.automate.name}"
+  aws_sshkey = "${var.aws_sshkey}"
+  contact_tag = "${var.contact_tag}"
+}
+
+module "blue-heart-workstations" {
+  source = "./workstations"
+  suit = "hearts"
+  color = "blue"
+  count = 13
+  automate_fqdn = "${aws_route53_record.automate.name}"
+  aws_sshkey = "${var.aws_sshkey}"
+  contact_tag = "${var.contact_tag}"
+}
+
+module "blue-diamonds-workstations" {
+  source = "./workstations"
+  suit = "diamonds"
+  color = "blue"
+  count = 13
+  automate_fqdn = "${aws_route53_record.automate.name}"
+  aws_sshkey = "${var.aws_sshkey}"
+  contact_tag = "${var.contact_tag}"
+}
+
+module "blue-spades-workstations" {
+  source = "./workstations"
+  suit = "spades"
+  color = "blue"
+  count = 13
+  automate_fqdn = "${aws_route53_record.automate.name}"
+  aws_sshkey = "${var.aws_sshkey}"
+  contact_tag = "${var.contact_tag}"
+}
+
+module "blue-clubs-workstations" {
+  source = "./workstations"
+  suit = "spades"
+  color = "blue"
+  count = 13
   automate_fqdn = "${aws_route53_record.automate.name}"
   aws_sshkey = "${var.aws_sshkey}"
   contact_tag = "${var.contact_tag}"
