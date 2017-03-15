4a2d4556dfd35868e6bd0abc86e9cba6600eb0ec
Jessica DeVita <jessica@chef.io>
2017-03-15 09:25:21 -0700

    refactored readme, setup variables file, etc
    Signed-off-by: Jessica DeVita jdevita@chef.io

--- workstations.tf
+++ workstations.tf
@@ -1,7 +1,7 @@
 module "heart-workstations" {
   source = "./workstations"
   suit = "hearts"
-  color = "orange"
+#  color = "orange"
   count = 13
   automate_fqdn = "${aws_route53_record.automate.name}"
   aws_sshkey = "${var.aws_sshkey}"
@@ -11,7 +11,7 @@ module "heart-workstations" {
 module "diamonds-workstations" {
   source = "./workstations"
   suit = "diamonds"
-  color = "orange"
+#  color = "orange"
   count = 13
   automate_fqdn = "${aws_route53_record.automate.name}"
   aws_sshkey = "${var.aws_sshkey}"
@@ -21,7 +21,7 @@ module "diamonds-workstations" {
 module "spades-workstations" {
   source = "./workstations"
   suit = "spades"
-  color = "orange"
+#  color = "orange"
   count = 13
   automate_fqdn = "${aws_route53_record.automate.name}"
   aws_sshkey = "${var.aws_sshkey}"
@@ -31,47 +31,7 @@ module "spades-workstations" {
 module "clubs-workstations" {
   source = "./workstations"
   suit = "clubs"
-  color = "orange"
-  count = 13
-  automate_fqdn = "${aws_route53_record.automate.name}"
-  aws_sshkey = "${var.aws_sshkey}"
-  contact_tag = "${var.contact_tag}"
-}
-
-module "blue-heart-workstations" {
-  source = "./workstations"
-  suit = "hearts"
-  color = "blue"
-  count = 13
-  automate_fqdn = "${aws_route53_record.automate.name}"
-  aws_sshkey = "${var.aws_sshkey}"
-  contact_tag = "${var.contact_tag}"
-}
-
-module "blue-diamonds-workstations" {
-  source = "./workstations"
-  suit = "diamonds"
-  color = "blue"
-  count = 13
-  automate_fqdn = "${aws_route53_record.automate.name}"
-  aws_sshkey = "${var.aws_sshkey}"
-  contact_tag = "${var.contact_tag}"
-}
-
-module "blue-spades-workstations" {
-  source = "./workstations"
-  suit = "spades"
-  color = "blue"
-  count = 13
-  automate_fqdn = "${aws_route53_record.automate.name}"
-  aws_sshkey = "${var.aws_sshkey}"
-  contact_tag = "${var.contact_tag}"
-}
-
-module "blue-clubs-workstations" {
-  source = "./workstations"
-  suit = "clubs"
-  color = "blue"
+#  color = "orange"
   count = 13
   automate_fqdn = "${aws_route53_record.automate.name}"
   aws_sshkey = "${var.aws_sshkey}"
